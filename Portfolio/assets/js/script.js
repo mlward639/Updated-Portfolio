385e44ef7ca44a3275e8e08a228a38ccafdc6ba5
Maggie Ward <mlward639@gmail.com>
2021-07-05 18:33:34 -0400

    updated colors and light/dark mode, still issues with last 3 work pics border & descriptions

--- Portfolio/assets/js/script.js
+++ Portfolio/assets/js/script.js
@@ -1,8 +1,34 @@
+var modeSwitch = document.querySelector(".mode-switch")
+var mode = true;
 
+console.log("mode  : ", mode)
 
 function lightMode() {
    var element = document.body;
    element.classList.toggle("light-mode");
-   var contactLinks = document.querySelector(".link-color-issue");
-   contactLinks.classList.toggle("light-mode-font");
+   var navbarContainer = document.querySelector(".navbar-container");
+   navbarContainer.classList.toggle("light-mode-other");
+   var aboutMeContainer = document.querySelector(".about-me");
+   aboutMeContainer.classList.toggle("light-mode-other");
+   var contactMeContainer = document.querySelector(".contact-me-container");
+   contactMeContainer.classList.toggle("light-mode-other");
+   var mainWorkDescription = document.querySelector(".main-work-description");
+   mainWorkDescription.classList.toggle("light-mode-description");
+   var otherWorkDescription = document.querySelector(".other-work-description");
+   otherWorkDescription.classList.toggle("light-mode-description");
+   var profilePic = document.querySelector(".profile-pic");
+   profilePic.classList.toggle("light-mode-pics");
+   var mainWorkPic = document.querySelector(".main-work-pic");
+   mainWorkPic.classList.toggle("light-mode-pics");
+   var workPic = document.querySelector(".work-pic");
+   workPic.classList.toggle("light-mode-pics");
+   if (mode === true){
+       mode = false;
+       console.log("mode  : ", mode);
+       modeSwitch.textContent = "Dark mode";
+   } else if (mode === false){
+       mode = true;
+       console.log("mode: ", mode)
+       modeSwitch.textContent = "Light mode";
+   }
 }
