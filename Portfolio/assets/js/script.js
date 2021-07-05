8fed1d0d5be393961e45eeaaca1b3c0ff42a931f
Maggie Ward <mlward639@gmail.com>
2021-07-05 16:15:24 -0400

    correct nav link format issue

--- Portfolio/assets/js/script.js
+++ Portfolio/assets/js/script.js
@@ -3,4 +3,6 @@
 function lightMode() {
    var element = document.body;
    element.classList.toggle("light-mode");
+   var contactLinks = document.querySelector(".link-color-issue");
+   contactLinks.classList.toggle("light-mode-font");
 }
