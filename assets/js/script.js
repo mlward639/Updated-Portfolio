3c57c0984cc189454cd5a6230f264586fbb0b40a
Maggie Ward <mlward639@gmail.com>
2021-07-06 19:53:04 -0400

    updating to ask Learning assistant about my JS line 17-18 issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,8 @@ function lightMode() {
    contactMeContainer.classList.toggle("light-mode-other");
    var mainWorkDescription = document.querySelector(".main-work-description");
    mainWorkDescription.classList.toggle("light-mode-description");
-   var otherWorkDescription = document.querySelector(".other-work-description");
-   otherWorkDescription.classList.toggle("light-mode-description");
+   var otherWorkDescription = document.getElementsByClassName("other-work-description");
+   otherWorkDescription.classList.toggle("light-mode-description"); //why does querySelector only apply to the first light-mode-description, not the other 3 in the class??? and querySelectorAll doesnt apply to any of them (and causes JS error with toggle)
    var profilePic = document.querySelector(".profile-pic");
    profilePic.classList.toggle("light-mode-pics");
    var mainWorkPic = document.querySelector(".main-work-pic");
