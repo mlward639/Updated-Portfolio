d3d1636751ce6a6678e593e8983f62b3e874488e
Maggie Ward <mlward639@gmail.com>
2021-07-06 20:45:43 -0400

    figured out workaround to light mode issues on my own

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,27 @@ function lightMode() {
    contactMeContainer.classList.toggle("light-mode-other");
    var mainWorkDescription = document.querySelector(".main-work-description");
    mainWorkDescription.classList.toggle("light-mode-description");
-   var otherWorkDescription = document.getElementsByClassName("other-work-description");
-   otherWorkDescription.classList.toggle("light-mode-description"); //why does querySelector only apply to the first light-mode-description, not the other 3 in the class??? and querySelectorAll doesnt apply to any of them (and causes JS error with toggle)
+   var otherWorkDescription1 = document.querySelector(".other-issue1");
+   otherWorkDescription1.classList.toggle("light-mode-description");
+   var otherWorkDescription2 = document.querySelector(".other-issue2");
+   otherWorkDescription2.classList.toggle("light-mode-description");
+   var otherWorkDescription3 = document.querySelector(".other-issue3");
+   otherWorkDescription3.classList.toggle("light-mode-description");
+   var otherWorkDescription4 = document.querySelector(".other-issue4");
+   otherWorkDescription4.classList.toggle("light-mode-description");
+   //otherWorkDescription.classList.toggle("light-mode-description"); //why does querySelector only apply to the first light-mode-description, not the other 3 in the class??? and querySelectorAll doesnt apply to any of them (and causes JS error with toggle)
    var profilePic = document.querySelector(".profile-pic");
    profilePic.classList.toggle("light-mode-pics");
    var mainWorkPic = document.querySelector(".main-work-pic");
    mainWorkPic.classList.toggle("light-mode-pics");
-   var workPic = document.querySelector(".work-pic");
-   workPic.classList.toggle("light-mode-pics");
+   var workPic1 = document.querySelector(".work-pic1");
+   workPic1.classList.toggle("light-mode-pics");
+   var workPic2 = document.querySelector(".work-pic2");
+   workPic2.classList.toggle("light-mode-pics");
+   var workPic3 = document.querySelector(".work-pic3");
+   workPic3.classList.toggle("light-mode-pics");
+   var workPic4 = document.querySelector(".work-pic4");
+   workPic4.classList.toggle("light-mode-pics");
    if (mode === true){
        mode = false;
        console.log("mode  : ", mode);
